5e4c25405c5334bd1372c0be62210edc6bf16101
rin <rinreg@inbox.ru>
2023-03-23 23:06:22 +0800

    Правка 1

---
+++ L3-Task1.py
@@ -0,0 +1,9 @@
+def my_div(arg_1, arg_2):
+    return arg_1 / arg_2
+
+var_1 = int(input("Введите числитель"))
+var_2 = int(input("Введите знаменатель"))
+
+if var_2 != 0:
+    print(f"Результат деления {my_div(var_1, var_2)}")
+орп проорр
